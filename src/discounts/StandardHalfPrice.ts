661985ed6441ca670aafe8c73c9f401f03dcace9
qiantang-itrazo <qian.tang@itrazotracetech.com>
2024-06-22 23:35:58 +1000

    implement customer discount manager

--- src/discounts/StandardHalfPrice.ts
+++ src/discounts/StandardHalfPrice.ts
@@ -1,3 +1,20 @@
-export default class StandardHalfPrice {
-    
+import { ItemType, PricingRule } from "../types/index";
+import { PREDEFINED_FULL_PRICES } from "../utils/constants";
+
+export default class StandardHalfPrice implements PricingRule {
+  private specialCustomers: Set<string>;
+
+  constructor(specialCustomers: string[]) {
+    this.specialCustomers = new Set(specialCustomers);
+  }
+
+  apply(items: ItemType[], customer: string) {
+    if (this.specialCustomers.has(customer)) {
+      const standardAdCount = items.filter(
+        (item) => item === ItemType.Standard
+      ).length;
+      return (standardAdCount * PREDEFINED_FULL_PRICES.standard) / 2;
+    }
+    return 0;
+  }
 }
