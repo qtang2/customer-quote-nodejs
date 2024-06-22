df647818bdc1f53cf7ccce663cdac7eefeaa2ded
qiantang-itrazo <qian.tang@itrazotracetech.com>
2024-06-22 23:32:37 +1000

    implement basic fix price discount

--- src/discounts/BasicFixedPrice.ts
+++ src/discounts/BasicFixedPrice.ts
@@ -1,3 +1,25 @@
-export default class BasicFixedPrice {
-    
+import { ItemType, PricingRule } from "../types/index";
+import {
+  BASIC_AD_DISCOUNT_PRICE,
+  PREDEFINED_FULL_PRICES,
+} from "../utils/constants";
+
+export default class BasicFixedPrice implements PricingRule {
+  private specialCustomers: Set<string>;
+
+  constructor(specialCustomers: string[]) {
+    this.specialCustomers = new Set(specialCustomers);
+  }
+
+  apply(items: ItemType[], customer: string) {
+    if (this.specialCustomers.has(customer)) {
+      const fixedPrice = items
+        .filter((item) => item === ItemType.Basic)
+        .reduce(() => {
+          return PREDEFINED_FULL_PRICES.basic - BASIC_AD_DISCOUNT_PRICE;
+        }, 0);
+      return fixedPrice;
+    }
+    return 0;
+  }
 }
