df7d4e4aa00ee1c2f9bba5b35737d88af4cd93ff
qiantang-itrazo <qian.tang@itrazotracetech.com>
2024-06-22 23:33:32 +1000

    implement standard two for one

--- src/discounts/StandardAdTwoForOne.ts
+++ src/discounts/StandardAdTwoForOne.ts
@@ -1,3 +1,20 @@
-export default class StandardAdTwoForOne {
-    
+import { ItemType, PricingRule } from "../types/index";
+import { PREDEFINED_FULL_PRICES } from "../utils/constants";
+
+export default class StandardAdTwoForOne implements PricingRule {
+  private specialCustomers: Set<string>;
+
+  constructor(specialCustomers: string[]) {
+    this.specialCustomers = new Set(specialCustomers);
+  }
+  apply(items: ItemType[], customer: string) {
+    if (this.specialCustomers.has(customer)) {
+      const standardAdCount = items.filter(
+        (item) => item === ItemType.Standard
+      ).length;
+      const discountCount = Math.floor(standardAdCount / 2);
+      return discountCount * PREDEFINED_FULL_PRICES.standard;
+    }
+    return 0;
+  }
 }
