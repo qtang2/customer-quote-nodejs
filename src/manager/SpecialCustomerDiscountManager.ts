f84426f7b61992b7937557c3b60b43f5ac9d48c6
qiantang-itrazo <qian.tang@itrazotracetech.com>
2024-06-22 23:36:53 +1000

    add special customer manager

--- src/manager/SpecialCustomerDiscountManager.ts
+++ src/manager/SpecialCustomerDiscountManager.ts
@@ -1,3 +1,22 @@
+import { PricingRule } from "../types/index";
+
 export default class SpecialCustomerDiscountManager {
-    
+  private customerDiscounts: Map<string, PricingRule[]> = new Map();
+
+  addSpecialCustomer(customer: string, rule: PricingRule) {
+    if (!this.customerDiscounts.has(customer)) {
+      this.customerDiscounts.set(customer, []);
+    }
+    this.customerDiscounts.get(customer)!.push(rule);
+  }
+
+  deleteSpecialCustomer(customer: string) {
+    if (!this.customerDiscounts.has(customer)) {
+      this.customerDiscounts.delete(customer);
+    }
+  }
+
+  getDiscountRules(customer: string): PricingRule[] {
+    return this.customerDiscounts.get(customer) || [];
+  }
 }
