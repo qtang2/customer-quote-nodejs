f2b26e1c369c8252e86c71e0f5ae43bad00fb954
qiantang-itrazo <qian.tang@itrazotracetech.com>
2024-06-22 23:27:07 +1000

    add model and discount class

---
+++ src/discounts/index.ts
@@ -0,0 +1,4 @@
+export {default as AddOnHalfPrice} from "./AddOnHalfPrice";
+export {default as BasicFixedPrice} from "./BasicFixedPrice";
+export {default as StandardAdTwoForOne} from "./StandardAdTwoForOne";
+export {default as StandardHalfPrice} from "./StandardHalfPrice";
